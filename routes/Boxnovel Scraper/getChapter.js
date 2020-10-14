7fd6d9c2d091e9fa155c6d4e93168d991f46ea3e
Jeffrey Huang <GamerJeffreyyT@gmail.com>
2020-10-13 23:50:52 -0400

    Added new method to scrape novel content from one shots, chapters that don't follow the standard url. ie. /chapter-1

--- routes/Boxnovel Scraper/getChapter.js
+++ routes/Boxnovel Scraper/getChapter.js
@@ -11,7 +11,8 @@ var router = express.Router();
 
 async function getChapterDetails(novel_url, chapter_number) {
     // Return content if found
-    let _id = url.parse(novel_url, true).pathname.split("/")[2] + "-chapter-" + chapter_number;
+    let novel_id = url.parse(novel_url, true).pathname.split("/")[2];
+    let _id = novel_id + "-chapter-" + chapter_number;
     var DATABASE_CHAPTER_DETAILS = {
         _id: _id,
         chapter_title: "",
@@ -21,57 +22,38 @@ async function getChapterDetails(novel_url, chapter_number) {
     const queryChapter = await client.db("NAMS").collection("CHAPTERS").findOne({ _id: DATABASE_CHAPTER_DETAILS._id });
     if (queryChapter == null) {
         let chapter_url = novel_url + "chapter-" + chapter_number;
+        const queryNovel = await client.db("NAMS").collection("NOVELS").findOne({ _id: novel_id });
+
         // Do the scrape
         // console.log(chapter_url);
-        await axios.get(url.parse(chapter_url, true)).then(async (response) => {
-            const $ = cheerio.load(response.data); // Load the page
-
-            if ($("div > div.reading-content > div") == null) {
-                return { Error: "No Content" };
-            }
-            console.log("Scraping for chapter content!");
-            // Get Chapter Title 1
-            if ($("div.cha-tit > h3").text() != "") {
-                DATABASE_CHAPTER_DETAILS.chapter_title = $("div > div.cha-tit > h3").text();
-                console.log("Chapter Title Selector 1: ", $("div > div.cha-tit > h3").text());
-            }
-            // Get Chapter Title 2
-            if ($("div.reading-content > div > p:nth-child(1) > strong").text() != "") {
-                DATABASE_CHAPTER_DETAILS.chapter_title = $("div.reading-content > div > p:nth-child(1) > strong").text();
-                console.log("Chapter Title Selector 2: ", $("div.reading-content > div > p:nth-child(1) > strong").text());
-            }
-
-            // Get Chapter Title 3
-            if ($("div.cha-tit.skiptranslate > div > h3").text() != "") {
-                DATABASE_CHAPTER_DETAILS.chapter_title = $("div.cha-tit.skiptranslate > div > h3").text();
-                console.log("Chapter Title Selector 3: ", $("div.cha-tit.skiptranslate > div > h3").text());
-            }
-
-            // Get Paragraphs
-            $("div.cha-content > div > p").each((index, p) => {
-                let paragraph = $(p).text();
-                DATABASE_CHAPTER_DETAILS.chapter_content.push(paragraph);
+        if (queryNovel.total_chapters == 1) {
+            var new_url;
+            await axios.get(url.parse(novel_url, true)).then(async (response) => {
+                const $ = cheerio.load(response.data);
+                $("div.page-content-listing.single-page > div > ul > li > a").each(async (index, element) => {
+                    // DATABASE_CHAPTER_DETAILS.chapter_content.push(paragraph);
+                    if ($(element).attr("href") != null) {
+                        new_url = $(element).attr("href");
+                    }
+                });
             });
-
-            // Get Paragraphs
-            $("div.reading-content > div > p").each((index, p) => {
-                let paragraph = $(p).text();
-                DATABASE_CHAPTER_DETAILS.chapter_content.push(paragraph);
+            await axios.get(new_url).then(async (response) => {
+                const $ = cheerio.load(response.data); // Load the page
+                await scrapeContent(DATABASE_CHAPTER_DETAILS, $);
             });
-
-            // Get Paragraphs
-            $("div.reading-content > div > div.cha-content > div").each((index, p) => {
-                let paragraph = $(p).text();
-                DATABASE_CHAPTER_DETAILS.chapter_content.push(paragraph);
+        } else {
+            await axios.get(url.parse(chapter_url, true)).then(async (response) => {
+                const $ = cheerio.load(response.data); // Load the page
+                await scrapeContent(DATABASE_CHAPTER_DETAILS, $);
             });
-        });
+        }
 
-        console.log("Checking for bad input - Title: ", DATABASE_CHAPTER_DETAILS.chapter_title);
+        console.log("Checking for bad input - Title: |", DATABASE_CHAPTER_DETAILS.chapter_title, "|");
         if (DATABASE_CHAPTER_DETAILS.chapter_title == "") {
             DATABASE_CHAPTER_DETAILS.chapter_title = `Chapter ${chapter_number}`;
         }
         if (DATABASE_CHAPTER_DETAILS.chapter_content.length == 0) return { Error: "We could not find the content." };
-        console.log("Seems like we don't have any bad input.");
+        // console.log("Seems like we don't have any bad input.");
         console.log("Adding chapter to our database -", DATABASE_CHAPTER_DETAILS.chapter_title, "Chapter", chapter_number);
         client
             .db("NAMS")
@@ -81,15 +63,49 @@ async function getChapterDetails(novel_url, chapter_number) {
                 console.log("[GetChapter - Save] Error found trying to add new novel entry.", err);
             });
 
-        console.log("Returning scraped content.");
+        // console.log("Returning scraped content.");
         return { chapter_title: DATABASE_CHAPTER_DETAILS.chapter_title, chapter_content: DATABASE_CHAPTER_DETAILS.chapter_content };
     } else {
         // We have the chapter in our database
         return { chapter_title: queryChapter.chapter_title, chapter_content: queryChapter.chapter_content };
     }
-    // Otherwise scrape for it.
-    // Scrape and send back the content
-    // Save it to our database.
+}
+
+async function scrapeContent(DATABASE_CHAPTER_DETAILS, $) {
+    // Get Chapter Title 1
+    if ($("div.cha-tit > h3").text() != "") {
+        DATABASE_CHAPTER_DETAILS.chapter_title = $("div > div.cha-tit > h3").text();
+        // console.log("Chapter Title Selector 1: ", $("div > div.cha-tit > h3").text());
+    }
+    // Get Chapter Title 2
+    if ($("div.reading-content > div > p:nth-child(1) > strong").text() != "") {
+        DATABASE_CHAPTER_DETAILS.chapter_title = $("div.reading-content > div > p:nth-child(1) > strong").text();
+        // console.log("Chapter Title Selector 2: ", $("div.reading-content > div > p:nth-child(1) > strong").text());
+    }
+
+    // Get Chapter Title 3
+    if ($("div.cha-tit.skiptranslate > div > h3").text() != "") {
+        DATABASE_CHAPTER_DETAILS.chapter_title = $("div.cha-tit.skiptranslate > div > h3").text();
+        // console.log("Chapter Title Selector 3: ", $("div.cha-tit.skiptranslate > div > h3").text());
+    }
+
+    // Get Paragraphs
+    $("div.cha-content > div > p").each((index, p) => {
+        let paragraph = $(p).text();
+        DATABASE_CHAPTER_DETAILS.chapter_content.push(paragraph);
+    });
+
+    // Get Paragraphs
+    $("div.reading-content > div > p").each((index, p) => {
+        let paragraph = $(p).text();
+        DATABASE_CHAPTER_DETAILS.chapter_content.push(paragraph);
+    });
+
+    // Get Paragraphs
+    $("div.reading-content > div > div.cha-content > div").each((index, p) => {
+        let paragraph = $(p).text();
+        DATABASE_CHAPTER_DETAILS.chapter_content.push(paragraph);
+    });
 }
 
 /**
@@ -107,149 +123,9 @@ router.post("/", async function (req, res, next) {
     let chapter_number = req.body.chapter_number;
     console.log(novel_url, chapter_number);
     // Find chapter in our database
+
     res.send(JSON.stringify(await getChapterDetails(novel_url, chapter_number)));
     res.end();
-    // const db_novel_title = url.parse(req.body.url, true).pathname.split("/")[2];
-    // const chapter_num = req.body.chapter_num;
-    // Check if we need to add to database
-    // console.log(req.body);
-    // if (!(await novelInDB(db_novel_title))) {
-    //     await axios
-    //         .get(url.parse(req.body.url.toString(), true))
-    //         .then(async (response) => {
-    //             const $ = cheerio.load(response.data); // Load the page
-    //             novel_details = {
-    //                 novel_title: req.body.novel_title,
-    //                 db_novel_title: db_novel_title,
-    //                 novel_url: req.body.url,
-    //                 total_chapters: 0,
-    //                 summary: [],
-    //                 cover_url: "https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image-300x225.png",
-    //             };
-    //             // -- Chapter Info -- //
-    //             // Get Total Chapters
-    //             novel_details.total_chapters = Number($(".wp-manga-chapter").first().text().trim().split(" ")[1]);
-    //             // Get Summary
-    //             $("#editdescription").each((index, element) => {
-    //                 novel_details.summary.push($(element).text());
-    //             });
-    //             // Get cover_url
-    //             $("div.summary_image > a > img").each((index, element) => {
-    //                 novel_details.cover_url = $(element).attr("src");
-    //             });
-    //             // -- Chapter Info End -- //
-    //             await addNewNovel(novel_details);
-    //         })
-    //         .catch((err) => {
-    //             console.log("Error found:", err);
-    //         });
-    // }
-    // // We now have a database entry
-    // // Time to add chapters i < schema.total_chapters + 1
-    // while (!(await novelInDB(db_novel_title))) {}
-    // let chapter_url;
-    // if (SAVE_ALL_CONTENT) {
-    //     for (let i = 1; i < schema.total_chapters + 1; i++) {
-    //         chapter_url = req.body.url.toString() + "/chapter-" + i;
-    //         await saveChapter(chapter_url, chapter_num, db_novel_title);
-    //     }
-    // } else {
-    //     chapter_url = req.body.url.toString() + "/chapter-" + chapter_num;
-    //     await saveChapter(chapter_url, chapter_num, db_novel_title);
-    // }
-    // res.send(JSON.stringify(await parseChapter(db_novel_title, chapter_num)));
-    // res.end();
 });
 
 module.exports = router;
-
-// async function parseChapter(db_novel_title, chapter_num) {
-//     const chapter = await client.db("NAMS").collection("chapters").findOne({ db_novel_title: db_novel_title, chapter: chapter_num });
-//     // Novel has to be on our database
-//     if (chapter_num == 0) {
-//         // Summary
-//         const novel = await client.db("NAMS").collection("novels").findOne({ db_novel_title: db_novel_title });
-//         return {
-//             content: novel.summary,
-//             chapter_title: novel.total_chapters,
-//         };
-//     }
-//     return {
-//         content: chapter.content,
-//         chapter_title: chapter.chapter_title,
-//     };
-// }
-
-// async function saveChapter(chapter_url, chapter_num, db_novel_title) {
-//     // The novel url base no /chapter-1/
-
-//     if (!(await chapterInDB(db_novel_title, chapter_num))) {
-//         // Chapter is not in database
-//         await axios
-//             .get(url.parse(chapter_url, true))
-//             .then(async (response) => {
-//                 const $ = cheerio.load(response.data); // Load the page
-
-//                 chapterObject = {
-//                     novel_title: db_novel_title,
-//                     chapter_title: "",
-//                     paragraphs: [],
-//                 };
-
-//                 // Get Chapter Title
-//                 if ($("div.reading-content > div > div.panel-heading").text() != null) {
-//                     chapterObject.chapter_title = $("div.reading-content > div > div.panel-heading").text();
-//                 } else if ($("div.reading-content > div > h3").text() == null) {
-//                     chapterObject.chapter_title = $("h4", ".reading-content").text();
-//                 } else {
-//                     console.log("Couldn't Find Chapter Title");
-//                 }
-
-//                 // Get Paragraphs
-//                 $("p", ".reading-content").each((index, p) => {
-//                     const paragraph = $(p).text();
-//                     chapterObject.paragraphs.push(paragraph);
-//                 });
-//                 await addChapterDetails(chapterObject, chapter_num);
-//             })
-//             .catch((err) => {
-//                 console.log("Error found:", err);
-//             });
-//     }
-// }
-
-// const schema = {
-//     novel_title: "",
-//     db_novel_title: "",
-//     cover_url: "",
-//     total_chapters: 0,
-//     summary: [],
-// };
-
-// async function chapterInDB(novel_title, chapter_num) {
-//     query = {
-//         db_novel_title: novel_title,
-//         chapter: chapter_num,
-//     };
-
-//     const chapter = await client.db("NAMS").collection("chapters").findOne(query);
-
-//     if (!chapter) {
-//         // The chapter is not in our database.
-//         return false;
-//     }
-//     // The chapter is in our database.
-//     return true;
-// }
-
-// async function addChapterDetails(chapt, chapter_num) {
-//     // Adding
-
-//     const chapter_details = {
-//         novel_title: chapt.novel_title,
-//         chapter_title: chapt.chapter_title,
-//         chapter: chapter_num,
-//         content: chapt.paragraphs,
-//     };
-//     await client.db("NAMS").collection("chapters").insertOne(chapter_details);
-// }
