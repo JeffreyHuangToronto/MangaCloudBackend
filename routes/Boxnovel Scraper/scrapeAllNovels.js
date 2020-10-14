5c90b6e6e3c4328c100658b2e6efed08c39fab07
Jeffrey Huang <GamerJeffreyyT@gmail.com>
2020-10-13 20:11:09 -0400

    Trying to scrape for novel titles differently.

--- routes/Boxnovel Scraper/scrapeAllNovels.js
+++ routes/Boxnovel Scraper/scrapeAllNovels.js
@@ -11,20 +11,31 @@ var router = express.Router();
 
 router.get("/", async function (req, res, next) {
     const PAGES = 63; // Manually Found
-    for (var i = 0; i <= PAGES; i++) {
+    for (var i = 0; i <= 2; i++) {
         console.log("Looking at page", i);
         await axios
             .get(`https://boxnovel.com/novel/page/${i}/?m_orderby=alphabet`)
             .then(async (response) => {
                 const $ = cheerio.load(response.data); // Load the page
-                $("div.post-title > h5 > a").each(async (index, element) => {
+                // $("div.post-title > h5 > a").each(async (index, element) => {
+                //     if ($(element).attr("href") != null) {
+                //         const body = {
+                //             novel_url: $(element).attr("href"),
+                //         };
+                //         // console.log("Sending Save:", body.novel_url);
+                //         await axios.post(api_url + "/scrape/boxnovel/noveldetails", body).catch(function (error) {
+                //             console.log("[ScrapeAllNovels1] Error found while sending request to scrape novel details.");
+                //         });
+                //     }
+                // });
+                $("div.post-title.font-title > h5 > a").each(async (index, element) => {
                     if ($(element).attr("href") != null) {
                         const body = {
                             novel_url: $(element).attr("href"),
                         };
                         // console.log("Sending Save:", body.novel_url);
                         await axios.post(api_url + "/scrape/boxnovel/noveldetails", body).catch(function (error) {
-                            console.log("[ScrapeAllNovels] Error found while sending request to scrape novel details.");
+                            console.log("[ScrapeAllNovels2] Error found while sending request to scrape novel details.", error);
                         });
                     }
                 });
